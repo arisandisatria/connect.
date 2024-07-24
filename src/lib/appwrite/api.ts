096ab4b8dd3655d9bcec4f3810f7bbfe7098390d
Airtas <89629005+ArisandiSatria@users.noreply.github.com>
2024-07-24 17:06:06 +0700

    fix bug

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,4 +1,4 @@
-import { ID, Query } from "appwrite";
+import { ID, ImageGravity, Query } from "appwrite";
 import { INewPost, INewUser, IUpdatePost } from "@/types";
 import { account, appwriteConfig, avatars, databases, storage } from "./config";
 
@@ -168,7 +168,7 @@ export function getFilePreview(fileId: string) {
       fileId,
       2000,
       2000,
-      "top",
+      ImageGravity.Top,
       100
     );
 
