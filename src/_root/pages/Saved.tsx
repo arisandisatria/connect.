e743b03a3c2d5aa7c4050ea2935ad2bb81dbbd7a
Airtas <89629005+ArisandiSatria@users.noreply.github.com>
2024-07-24 23:42:17 +0700

    fix typo

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -1,7 +1,6 @@
 import GridPostList from "@/components/shared/GridPostList";
 import Loader from "@/components/shared/Loader";
 import { useGetSavedPosts } from "@/lib/react-query/queriesAndMutations";
-import { Models } from "appwrite";
 
 const Saved = () => {
   const { data: savedPosts, isPending: isSavedPostsLoading } =
