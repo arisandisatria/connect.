fc81f23e98137671bff0936ab2772b21a0e9b9f4
Airtas <89629005+ArisandiSatria@users.noreply.github.com>
2024-07-24 16:54:10 +0700

    initial commit

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+export const SignUpValidationSchema = z.object({
+  name: z.string().min(2, { message: "Too short!" }),
+  username: z.string().min(2, { message: "Too short!" }).max(50),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters!" }),
+});
+
+export const SignInValidationSchema = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters!" }),
+});
+export const PostValidationSchema = z.object({
+  caption: z.string().min(5).max(2200),
+  file: z.custom<File[]>(),
+  location: z.string().min(2).max(100),
+  tags: z.string(),
+});
